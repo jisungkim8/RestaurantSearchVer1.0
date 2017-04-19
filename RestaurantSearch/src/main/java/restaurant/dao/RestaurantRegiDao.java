111ef31a69d83df676a8066f76cb9f4173fc7279
jason park <zeipar@gmail.com>
2017-04-19 15:56:50 +0900

    PJS 식당등록 사진 경로수정

--- RestaurantSearch/src/main/java/restaurant/dao/RestaurantRegiDao.java
+++ RestaurantSearch/src/main/java/restaurant/dao/RestaurantRegiDao.java
@@ -2,6 +2,7 @@ package restaurant.dao;
 
 import restaurant.dto.RestaurantDto;
 import restaurant.dto.ShopDetInfoDto;
+import restaurant.dto.ShopPhotoDto;
 
 public interface RestaurantRegiDao {
 	public void insertRestaurant(RestaurantDto restaurantDto);
@@ -12,10 +13,12 @@ public interface RestaurantRegiDao {
 
 	public void insertRestaurantDetInfo(ShopDetInfoDto shopDetInfoDto);
 	
+	public void insertRestaurantPhoto(ShopPhotoDto shopPhotoDto);
+	
 	public void deleteRestaurant(String restaurantId);
 
 	public int countRestaurantId();
-	
+
 /*	public RestaurantDto selectMemDetInfo(RestaurantDto restaurantDto);
 
 	public RestaurantDto searchMemDetInfo(RestaurantDto restaurantDto);
